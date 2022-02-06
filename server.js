9c86a2f4a23bb36b5da5c190e3ad974c1d8c2499
Robert Albenda <ralbenda@gmail.com>
2022-02-06 15:04:30 -0500

    hw added db.js

--- server.js
+++ server.js
@@ -1,67 +1,78 @@
 console.log('hello world');
-const pg = require ('pg');
+const {client, synchAndSeed} = require('./db.js')
 const express = require('express')
 const app = express();
 
 app.get('/', async (req, res,next) => {
    try{ 
        const response= await client.query('Select * FROM lawyers');
-       res.send(response.rows)
+       const lawyers = response.rows
+       res.send(`
+       <html>
+            <head>
+            <style>
+            body{
+                background-color:aquamarine;
+            }
+            h1 {
+                font-family:verdana;
+                padding:1rem;
+                
+            }
+            h2 {
+                font-family:verdana;
+                padding:1rem;
+            }
+            li>a{
+                font-family:verdana;
+                padding:1rem;
+                color:red;
+                font-weight:bold;
+            }
+            </style>
+                <body>
+                    <h1>
+                        Robby's Law Firm
+                    </h1>
+                    <h2>
+                        LAWYER LIST
+                    </h2>
+                    <ul>
+                    ${
+                        lawyers.map(lawyer => `
+                        <li>
+                        <a href = 'lawyers/${lawyer.name}'>
+                            ${lawyer.name}
+                        </a>
+                        </li>
+                        `).join('')
+                    }
+                    
+                    </ul>
+                </body>
+            </head>
+       </html>
+       `)
    }
 
 
    catch (ex){
-        console.log(ex)
+        next(ex)
    }
 })
+
+
+
 
 const port = process.env.PORT || 3000;
 
 
-const client = new pg.Client('postgres://localhost/robbys_law_firm_db');
-
-const syncAndSeed = async() => {
-    const SQL = `
-
-        DROP TABLE IF EXISTS job_title; 
-        DROP TABLE IF EXISTS lawyers;
-        
-        CREATE TABLE lawyers(
-            id INTEGER PRIMARY KEY,
-            name VARCHAR(100) NOT NULL
-
-        );
-
-        CREATE TABLE job_title(
-            id INTEGER PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
-            lawyer_id INTEGER REFERENCES lawyers(id)
-        );
-
-        INSERT INTO lawyers (id,name) VALUES (1,'amy');
-        INSERT INTO lawyers (id,name) VALUES (2,'elaine');
-        INSERT INTO lawyers (id,name) VALUES (3,'abe');
-        INSERT INTO lawyers (id,name) VALUES (4,'louise');
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (1, 'defense_attorney', 1);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (2, 'party_planner', 1);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (3, 'prosecutor', 2);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (4, 'social_media_director', 2);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (5, 'entertainment_attorney', 3);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (6, 'HR_director', 3);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (7, 'tax_attorney', 4);
-        INSERT INTO job_title(id,name,lawyer_id) VALUES (8, 'website_manager', 4);    
-
-    
-    `;
-    await client.query(SQL);
-
-};
 
 const setup = async() => {
 try{
 
     await client.connect();
-    await syncAndSeed()
+    await synchAndSeed()
     console.log('connected to database');
     app.listen(port,() => {
         console.log(`listening on port ${port}`);
