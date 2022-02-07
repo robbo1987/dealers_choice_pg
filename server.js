c0c4c05629b862c020a5580eb567e22c8289e161
Robert Albenda <ralbenda@gmail.com>
2022-02-07 13:03:04 -0500

    hw updates

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.get('/', async (req, res,next) => {
             }
             li>a{
                 font-family:verdana;
-                padding:1rem;
                 color:red;
                 font-weight:bold;
             }
@@ -41,7 +40,7 @@ app.get('/', async (req, res,next) => {
                     ${
                         lawyers.map(lawyer => `
                         <li>
-                        <a href = 'lawyers/${lawyer.name}'>
+                        <a href = 'lawyers/${lawyer.id}'>
                             ${lawyer.name}
                         </a>
                         </li>
@@ -61,7 +60,67 @@ app.get('/', async (req, res,next) => {
    }
 })
 
-
+app.get('/lawyers/:id', async (req, res,next) => {
+    try{ 
+        let response= await client.query('Select * FROM lawyers WHERE id=$1',[req.params.id]);
+        const lawyers = response.rows[0]
+        response = await client.query('SELECT job_title.name AS jobName, job_title.lawyer_id, lawyers.* FROM lawyers JOIN job_title ON job_title.lawyer_id = lawyers.id WHERE lawyers.id=$1',[req.params.id]);
+        const jobs = response.rows
+        res.send(`
+        <html>
+             <head>
+             <style>
+             body{
+                 background-color:aquamarine;
+             }
+             h1 {
+                 font-family:verdana;
+                 padding:1rem;
+                 
+             }
+             h2 {
+                 font-family:verdana;
+                 padding:1rem;
+             }
+             li{
+                 font-family:verdana;
+                 padding:1rem;
+                 color:red;
+                 font-weight:bold;
+             }
+             </style>
+                 <body>
+                     <h1>
+                        Robby's Law Firm
+                     </h1>
+                     <h2>
+                        Featured Attorney
+                     </h2>
+                    
+                     <ul>
+                     ${
+                        jobs.map(job => `
+                        <li>
+                            ${job.jobname}
+                        
+                        </li>
+                        `).join('')
+                    }
+                    </ul>
+                    <h2>
+                    <a href = '/'> Click Here to Return to Homepage </a>
+                    </h2>
+                 </body>
+             </head>
+        </html>
+        `)
+    }
+ 
+ 
+    catch (ex){
+         next(ex)
+    }
+ })
 
 
 const port = process.env.PORT || 3000;
